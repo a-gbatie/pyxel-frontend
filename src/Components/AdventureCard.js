87aab3117deb7fc27b8ed63ddf633cc366153102
Adan Escamilla <adanescamilla580@gmail.com>
2021-03-29 23:18:34 -0700

    component setup

---
+++ src/Components/AdventureCard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class AdventureCard extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
